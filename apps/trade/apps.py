4f2e6a7881c380ac78da336e1d7e6878db13cf45
shao-169 <842532954@qq.com>
2022-04-14 16:41:12 +0800

    devops:v1.3

---
+++ apps/trade/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class TradeConfig(AppConfig):
+    name = 'trade'
+    verbose_name = "订单信息"
+    app_label= "订单信息"
+
---
+++ apps/users/adminx.py
@@ -0,0 +1,49 @@
+
+# _*_ encoding:utf-8 _*_
+import xadmin
+from xadmin import views
+from xadmin.plugins.auth import UserAdmin
+from .models import UserProfile,EmailVerifyRecord,Banner
+from trade.models import Order,OrderDetail,Cart
+from teachers.models import Teacher
+from courses.models import Course,Category,Lesson,Video
+
+
+__author__ = 'YZF'
+__date__ = '2018/3/14,13:59'
+
+
+# ----- adminx 全局配置
+
+
+class BaseSetting:
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobalSettings:
+    site_title = 'SLTP后台管理系统'
+    site_footer = 'SLTP'
+    global_models_icon = {
+        UserProfile: "glyphicon glyphicon-user",Order:'glyphicon glyphicon-shopping-cart',
+        OrderDetail:'glyphicon glyphicon-shopping-cart',Cart:'glyphicon glyphicon-shopping-cart',
+        Teacher:'glyphicon glyphicon-star-empty',Course:'glyphicon glyphicon-book',Lesson:'glyphicon glyphicon-book',
+        Video:'glyphicon glyphicon-book',
+        Category:'glyphicon glyphicon-book'
+    }  # 设
+    menu_style = 'accordion'
+
+# ------
+
+
+class EmailVerifyRecordAdmin:
+    pass
+
+
+class BannerAdmin:
+    pass
+
+xadmin.site.register(views.CommAdminView,GlobalSettings)
+xadmin.site.register(Banner,BannerAdmin)
+xadmin.site.register(views.BaseAdminView,BaseSetting)
+
